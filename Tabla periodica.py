bf364fde471a3add87290d5cf091bc2632df853a
GerhardFreiberg <39864702+GerhardFreiberg@users.noreply.github.com>
2018-06-02 22:39:20 -0500

    Update Tabla periodica.py

--- Tabla periodica.py
+++ Tabla periodica.py
@@ -2,16 +2,19 @@ numero = 0
 simbolo = 0
 nombre = 0
 categoria = 0
-radiactivo = 0
+radiactivo = [43,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118]
 peso = 0
-# Metales Alcalinos = ma
-# metales de transición = mt
-# metales alcalinotérreos mat
-# metaloide = ml
-# semiconductores = sm
-# Actinidos = ac
-# No Metal = nm
-# Halógenos = ha
-# lantánidos = la
-# Gases Nobles = gn
-
+#Metales Alcalinos = ma
+#metales de transicion = mt
+#metales alcalinoterreos mat
+#metaloide = ml
+#semiconductores = sm
+#Actinidos = ac
+#No Metal = nm
+#Halogenos = ha
+#lantanidos = la
+#Gases Nobles = gn
+entra = input(int)
+if entra == (radiactivo):
+    print("si")
+else: print("no")
