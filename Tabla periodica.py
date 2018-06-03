957f823aac3c0655671ffe125c6150c6752a2244
GerhardFreiberg <39864702+GerhardFreiberg@users.noreply.github.com>
2018-06-02 21:59:45 -0500

    Update Tabla periodica.py

--- Tabla periodica.py
+++ Tabla periodica.py
@@ -1,1 +1,17 @@
-print("demo")
+numero = 0
+simbolo = 0
+nombre = 0
+categoria = 0
+radiactivo = 0
+peso = 0
+# Metales Alcalinos = ma
+# metales de transición = mt
+# metales alcalinotérreos mat
+# metaloide = ml
+# semiconductores = sm
+# Actinidos = ac
+# No Metal = nm
+# Halógenos = ha
+# lantánidos = la
+# Gases Nobles = gn
+
