b61aa629953830bf249ec934c1c5433e4a4cd178
ksuo <40323744+ksuo@users.noreply.github.com>
2018-06-16 15:08:58 -0500

    Update El-autocorrector.py

--- El-autocorrector.py
+++ El-autocorrector.py
@@ -1,1 +1,24 @@
-print("hello world")
+def crearArchivo():
+    archivo = open("diccito.txt","w")
+    archivo.close()
+def escribirArchivo():
+    archivo = open("diccito.txt","a")
+    archivo.write('nuestro\n')
+    archivo.write('burbuja\n')
+    archivo.write('puesto\n')
+    archivo.write('cuido\n')
+    archivo.write('corazon\n')
+    archivo.write('caracol\n')
+    archivo.write('nuesro\n')
+    archivo.write('permiso\n')
+    archivo.write('casa\n')
+    archivo.write('luz\n')
+    archivo.close
+def leerArchivo():
+    archivo = open("diccito.txt","r")
+    linea = archivo.readline()
+    while linea != "":
+        print(linea)
+        linea = archivo.readline()
+    archivo.close()
+leerArchivo()
