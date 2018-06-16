a21c425891661a7695ae35aa8f490f394ab72950
dianamp10 <40323746+dianamp10@users.noreply.github.com>
2018-06-16 17:03:18 -0500

    Update El-autocorrector.py

--- El-autocorrector.py
+++ El-autocorrector.py
@@ -1,21 +1,5 @@
-def crearArchivo():
-    archivo = open("C:\data.txt","w")
-    archivo.close()
-def escribirArchivo():
-    archivo = open("C:\data.txt","a")
-    archivo.write('nuestro\n')
-    archivo.write('burbuja\n')
-    archivo.write('puesto\n')
-    archivo.write('cuido\n')
-    archivo.write('corazon\n')
-    archivo.write('caracol\n')
-    archivo.write('nuesro\n')
-    archivo.write('permiso\n')
-    archivo.write('casa\n')
-    archivo.write('luz\n')
-    archivo.close
 def leerArchivo():
-    archivo = open("C:\data.txt","r")
+    archivo = open("C:\diccito.txt.dic", "r")
     linea = archivo.readline()
     while linea != "":
         print(linea)
