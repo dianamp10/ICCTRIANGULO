ae325805be534dec87d18a3366a227639ed19831
ksuo <40323744+ksuo@users.noreply.github.com>
2018-06-26 16:29:06 -0500

    Update triángulo.py

--- triángulo.py
+++ triángulo.py
@@ -1,7 +1,5 @@
-#Idea: Crear una calculadora de triangulos rectángulos
 import math
 print("Datos del triángulo rectángulo")
-#Crear variables tipo base, altura, perímerto, hipotenusa 
 base = 0
 altura = 0
 perimetro = 0
@@ -10,8 +8,6 @@ base = int(input("Ingrese la base : "))
 altura = int(input("Ingrese la altura: "))
 hipotenusa = math.sqrt((base**2)+(altura**2))
 perimetro = str(base+altura+hipotenusa)
-#hacer un triángulo gráficamente
-#con simbolos
 print("     /|")
 print(("    / |<-")+(" (")+str(altura)+(")"))
 print(("   /<-|---")+(" (")+str(hipotenusa)+(")"))
